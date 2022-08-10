0cae09dbbb9d3e9466efe191a326714da1ee6f23
shekhar <shekharjha7417870578@gmail.com>
2022-08-10 21:21:45 +0530

    Added util class

---
+++ src/com/bridgelabz/junit/Date.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.junit;
+import java.util.Scanner;
+public class Date {
+
+
+
+
+        public static void main(String[] args) {
+            int d, m, y;
+            Scanner s = new Scanner(System.in);
+            System.out.println("enter date in day month year:");
+            d = s.nextInt();
+            m = s.nextInt();
+            y = s.nextInt();
+            System.out.println("day is "+util.DayOfWeek(d, m, y));
+            s.close();
+
+        }
+    }
+
